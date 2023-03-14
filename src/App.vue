26a9276215bb98787996ebe52044f37e1709fae0
Benjamin <benjamindeltour22@gmail.com>
2023-03-14 11:45:46 +0100

    add new task

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>To Do List</h1>
+  <Todo />
 </template>
 
 <style scoped>
@@ -21,9 +14,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/Todo.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: {}
+    }
+  },
+  methods: {
+    addTodo: function (form) {
+      form.preventDefault()
+      let date = new Date()
+      date = date.toLocaleString('fr-FR')
+      this.newTodo = {
+        title: this.newTodo.title,
+        createdAt: date,
+        done: false
+      }
+      this.todos.push(this.newTodo)
+      this.newTodo = {}
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <form @submit="addTodo">
+      <input v-model="newTodo.title" placeholder="Add Task">
+      <button type="submit">Add</button>
+    </form>
+    <div class="list">
+      <div class="todo" v-for="(todo, index) in todos">
+        <p>{{ todo.title }}</p>
+        <button>Done</button>
+      </div>
+    </div>
+    <div class="complete">
+
+    </div>
+
+  </div>
+</template>
