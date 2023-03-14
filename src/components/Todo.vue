cd084a9a2cffd280cd8e404e81ae7d8ae2c86ed6
Benjamin <benjamindeltour22@gmail.com>
2023-03-14 16:02:11 +0100

    add edit task

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,7 +3,10 @@ export default {
   data() {
     return {
       todos: [],
-      newTodo: {}
+      newTodo: {},
+      isEditing: false,
+      indexEdit: 0,
+      buttonText: 'Add'
     }
   },
   methods: {
@@ -11,20 +14,34 @@ export default {
       form.preventDefault()
       if (this.newTodo.title === ' ' || this.newTodo.title === '' || this.newTodo.title === undefined)
         return
-      let date = new Date()
-      date = date.toLocaleString('fr-FR')
-      this.newTodo = {
-        title: this.newTodo.title,
-        createdAt: date,
-        done: false
+      if (!this.isEditing) {
+        let date = new Date()
+        date = date.toLocaleString('fr-FR')
+        this.newTodo = {
+          title: this.newTodo.title,
+          createdAt: date,
+          done: false
+        }
+        this.todos.push(this.newTodo)
+        this.newTodo = {}
       }
-      this.todos.push(this.newTodo)
-      this.newTodo = {}
+      if (this.isEditing) {
+        this.todos[this.indexEdit].title = this.newTodo.title
+        this.newTodo = {}
+        this.buttonText = 'Add'
+      }
     },
     changeTodo: function (todo) {
       if (todo.done)
         todo.done = false
       else todo.done = true
+    },
+    updateTodo: function (index) {
+      const todo = this.todos[index]
+      this.newTodo.title = todo.title
+      this.isEditing = true
+      this.indexEdit = index
+      this.buttonText = 'Edit'
     },
     deleteTodo: function (todo) {
       const listTodos = this.todos.filter(element => element != todo)
@@ -45,12 +62,13 @@ export default {
   <div class="card">
     <form @submit="addTodo">
       <input v-model="newTodo.title" placeholder="Add Task">
-      <button type="submit">Add</button>
+      <button type="submit">{{ buttonText }}</button>
     </form>
     <div class="list">
-      <div class="todo" v-for="todo in todos">
+      <div class="todo" v-for="(todo, index) in todos">
         <p :class="{ done: todo.done }">{{ todo.title }}</p>
         <button @click="changeTodo(todo)">Done</button>
+        <button @click="updateTodo(index)">Update</button>
         <button @click="deleteTodo(todo)">Delete</button>
       </div>
     </div>
